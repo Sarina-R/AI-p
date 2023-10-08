1f6fefb5899deb5e35de410d3b6d306689ea20ed
Sarina-R <146827892+Sarina-R@users.noreply.github.com>
2023-10-08 14:23:57 +0330

    add possibility

--- src/container/posibility/Posibility.jsx
+++ src/container/posibility/Posibility.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-import './posibility.css'
+import React from "react";
+import "./posibility.css";
+import possibilityImage from "../../assets/possibility.png";
 const Posibility = () => {
   return (
-    <div>Posibility</div>
-  )
-}
+    <section className="section-p" id="possibility">
+      <div className="left-p">
+        <img src={possibilityImage} />
+      </div>
+      <div className="right">
+        <p>Request Early Access to Get Started</p>
+        <h1>The possibilities are beyond your imagination</h1>
+        <p className="weight">
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <p className="orange">Request Early Access to Get Started</p>
+      </div>
+    </section>
+  );
+};
 
-export default Posibility
+export default Posibility;
