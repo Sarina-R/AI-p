81c52dcd66fd7ca51a64cfd87e20ce9ce6f19d8b
Sarina-R <146827892+Sarina-R@users.noreply.github.com>
2023-10-09 06:02:38 +0330

    add cta

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import './cta.css'
+import React from "react";
+import "./cta.css";
 
 const Cta = () => {
   return (
-    <div>Cta</div>
-  )
-}
+    <div className="cta">
+      <div className="left-c">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className="right-c">
+        <button type="button">get started</button>
+      </div>
+    </div>
+  );
+};
 
-export default Cta
+export default Cta;
