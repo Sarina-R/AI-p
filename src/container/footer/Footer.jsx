aa1630743d3c463bc05d3f0e09f046f939d860bd
Sarina-R <146827892+Sarina-R@users.noreply.github.com>
2023-10-10 14:27:46 +0330

    add footer

--- src/container/footer/Footer.jsx
+++ src/container/footer/Footer.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
+import logo from "../../assets/logo.svg";
 
 const footer = () => {
   return (
-    <div>footer</div>
-  )
-}
+    <section className="section-f">
+      <div className="top-f">
+        <h1>Do you want to step in to the future before others</h1>
+        <button type="button">Request Early Access</button>
+      </div>
+      <div className="bottom-f">
+        <div className="left-f">
+          <img src={logo} />
+          <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+        </div>
+        <div className="right-f">
+          <div className="right-f-a">
+            <h4>Links</h4>
+            <p>Overons</p>
+            <p>Social Media</p>
+            <p>Counters</p>
+            <p>Contact</p>
+          </div>
+          <div className="right-f-b">
+            <h4>Company</h4>
+            <p>Terms & Conditions</p>
+            <p>Privacy Policy</p>
+            <p>Contact</p>
+          </div>
+          <div className="right-f-c">
+            <h4>Get in touch</h4>
+            <p>Crechterwoord K12 182 DK Alknjkcb</p>
+            <p>09938023855</p>
+            <p>Sarinarezanezhad@gmail.com</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default footer
+export default footer;
