c2a6d8dd8bf0dd42e957f8500cd566b7ebba7975
Sarina-R <146827892+Sarina-R@users.noreply.github.com>
2023-10-20 22:44:41 +0330

    fix link

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/AI-p" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
